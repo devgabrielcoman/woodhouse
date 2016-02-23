8d56509c1ace48ee70f65ff24e8c643ebb24661e
Gabriel Coman <dev.gabriel.coman@gmail.com>
2016-02-23 23:07:33 +0000

    First

---
+++ Woodhouse/Example/Pods/Target Support Files/Woodhouse/Woodhouse-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Woodhouse : NSObject
+@end
+@implementation PodsDummy_Woodhouse
+@end
---
+++ Woodhouse/Example/Pods/Target Support Files/Woodhouse/Woodhouse-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double WoodhouseVersionNumber;
+FOUNDATION_EXPORT const unsigned char WoodhouseVersionString[];
+
